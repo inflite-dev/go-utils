98c66d4bc69ef2f1bae8a7fc681b4ec0fe1cfa8c
francoposa <franco@francoposa.io>
2025-02-24 05:43:33 -0800

    fix up db/postgres RandomDBName

--- db/postgres/fixtures.go
+++ db/postgres/fixtures.go
@@ -3,16 +3,14 @@ package postgres
 import (
 	"fmt"
 	"math/rand"
-	"time"
 )
 
-// RandomDBName generates Postgres database names with a common prefix
-// in order to avoid collisions and race conditions in parallel tests
-func RandomDBName(dbNamePrefix string) string {
-	rand.Seed(time.Now().UnixNano())
-	return fmt.Sprintf(
-		"%s_%d",
-		dbNamePrefix,
-		rand.Int(),
-	)
+// RandomDBName generates a Postgres database name with a provided prefix and random suffix
+// in order to avoid collisions and race conditions in parallel tests.
+//
+// This uses a pseudorandom integer rather than a UUID to avoid hyphens in the database name.
+// Postgres database names with hyphens do not autocomplete on the psql command line,
+// which is a pain in case you get stuck manually deleting a bunch of these.
+func RandomDBName(prefix string) string {
+	return fmt.Sprintf("%s_%d", prefix, rand.Int())
 }
